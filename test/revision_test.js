492e6e7a33a8fbe8a6bd3c4792b85899dd44cfe7
Xavier Dutreilh <xavier@dutreilh.com>
2016-03-06 21:19:18 +0100

    Use ES6 features in test

--- test/revision_test.js
+++ test/revision_test.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var fs = require('fs');
+const fs = require('fs');
 
 function read(filename) {
   return fs.readFileSync(filename, {'encoding': 'utf8'});
 }
 
 exports.revision = {
-  'build': function(test) {
-    var actual = read('tmp/index.html');
-    var expected = read('test/expected/index.html');
+  'build': (test) => {
+    const actual = read('tmp/index.html');
+    const expected = read('test/expected/index.html');
 
     test.equal(actual, expected, 'should revision static files');
 
