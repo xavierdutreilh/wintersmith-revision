5b60fcce389d021d7ebc01dc47052cd1e064ec1d
Xavier Dutreilh <xavier@dutreilh.com>
2016-06-07 21:50:05 +0200

    Refactor test

--- test/revision_test.js
+++ test/revision_test.js
@@ -1,15 +1,16 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 function read(filename) {
-  return fs.readFileSync(filename, {'encoding': 'utf8'});
+  return fs.readFileSync(filename, 'utf8');
 }
 
 exports.revision = {
   'build': (test) => {
-    const actual = read('tmp/index.html');
-    const expected = read('test/expected/index.html');
+    const actual = read(path.join(__dirname, '..', 'tmp', 'index.html'));
+    const expected = read(path.join(__dirname, 'expected', 'index.html'));
 
     test.equal(actual, expected, 'should revision static files');
 
