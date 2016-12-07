02f2e86b077c74cb61d30ca2c989145dd4a0b6c1
Xavier Dutreilh <xavier@dutreilh.com>
2016-12-07 17:12:33 +0100

    Stop using path module

--- test/revision_test.js
+++ test/revision_test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
 
 function read(filename) {
   return fs.readFileSync(filename, 'utf8');
@@ -9,8 +8,8 @@ function read(filename) {
 
 exports.revision = {
   'build': (test) => {
-    const actual = read(path.join(__dirname, '..', 'tmp', 'index.html'));
-    const expected = read(path.join(__dirname, 'expected', 'index.html'));
+    const actual = read(`${__dirname}/../tmp/index.html`);
+    const expected = read(`${__dirname}/expected/index.html`);
 
     test.equal(actual, expected, 'should revision static files');
 
